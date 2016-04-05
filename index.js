9d20a7e3b18d935860091959b67f9e9e000fde0e
Adam Rezich <adam@rezich.com>
2016-04-05 02:27:36 -0700

    added shortid, missing index.html

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var shortid = require('shortid');
+
+var open = [];
+var connections = []
 
 app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname });
 });
 
 io.on('connection', function(socket){
+  console.log('user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
